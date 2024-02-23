8eec010df76a443d85f4d3a55936f16a1678c52b
Umut Gülaç <156457615+justalingunaut@users.noreply.github.com>
2024-02-23 08:03:13 +0300

    Add files via upload

---
+++ todoapp1/todoapp1.py
@@ -0,0 +1,28 @@
+yapilacaklar = []
+
+while True:
+    komut = input("Göster, ekle, düzenle, tamamla ya da çıkış yazınız: ")
+    komut = komut.strip()
+
+    match komut:
+        case 'ekle':
+            yapilacak = input("Yapılacak listesine ekleme yapınız:")
+            yapilacaklar.append(yapilacak)
+        case 'göster':
+            for index, item in enumerate(yapilacaklar):
+                row = (f"{index + 1}-{item}")
+                print (row)
+        case 'edit':
+            numara = int(input("Düzenlenecek numarayı giriniz: "))
+            numara = numara - 1
+            yeni_yapilacak = input("Yeni girdi yazınız: ")
+            yapilacaklar[numara] = yeni_yapilacak
+
+        case 'çıkış':
+            break
+
+        case 'tamamla':
+            numara = int(input("Tamamlanacak görevin numarasını giriniz: "))
+            yapilacak.pop(numara)
+
+print("Hoşçakal!")
